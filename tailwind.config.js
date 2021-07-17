48fa1e69fa4261e00c4b67fb28fb389e1002fb5f
joseluis <jdesignermayor@gmail.com>
2021-07-17 14:38:53 -0500

    header style, add font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,34 @@ const { guessProductionMode } = require("@ngneat/tailwind");
 process.env.TAILWIND_MODE = guessProductionMode() ? 'build' : 'watch';
 
 module.exports = {
-    prefix: '',
-    mode: 'jit',
-    purge: {
-      content: [
-        './src/**/*.{html,ts,css,scss,sass,less,styl}',
-      ]
+  prefix: '',
+  mode: 'jit',
+  purge: {
+    content: [
+      './src/**/*.{html,ts,css,scss,sass,less,styl}',
+    ]
+  },
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    height: {
+      "10v": "10vh",
+      "20v": "20vh",
+      "30v": "30vh",
+      "40v": "40vh",
+      "50v": "50vh",
+      "60v": "60vh",
+      "70v": "70vh",
+      "80v": "80vh",
+      "90v": "90vh",
+      "100v": "100vh",
     },
-    darkMode: 'class', // or 'media' or 'class'
-    theme: {
-      extend: {},
+    color: {
+      primary: "#FFB74D"
     },
-    variants: {
-      extend: {},
-    },
-    plugins: [],
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 };
