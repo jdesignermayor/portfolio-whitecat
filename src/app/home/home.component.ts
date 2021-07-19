2d768049f61c554b1b0f0ca985c0496212c407d5
joseluis <jdesignermayor@gmail.com>
2021-07-19 18:12:14 -0500

    modify home and introduction component for merge functionality

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+import { DOCUMENT } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+  }
+}
