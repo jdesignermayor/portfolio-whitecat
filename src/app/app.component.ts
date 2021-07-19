c88b63124c7d366cb5dbffb09c252b19361aec54
joseluis <jdesignermayor@gmail.com>
2021-07-19 11:31:47 -0500

    add smooth scrollbar in the home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, ElementRef, EventEmitter, Inject, ViewChild } from '@angular/core';
+import Scrollbar from 'smooth-scrollbar';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio-whitecat';
+  @ViewChild('mainContainer') mainContainer: ElementRef | undefined;
+
+  constructor(@Inject(DOCUMENT) private document: Document) {
+
+  }
+
+  ngAfterViewInit(): void {
+
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+
+    if (this.mainContainer) {
+      Scrollbar.init(this.mainContainer.nativeElement, {
+        plugins: {
+          overscroll: true
+        }
+      });
+    } else {
+      alert("doenst work")
+    }
+  }
 }
